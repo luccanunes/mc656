9802f14227c5dc035f30075d3ac63133bcfdf315
Lucca Nunes <lucca.nunes@ifood.com.br>
2024-09-26 23:45:40 -0300

    fix pipeline

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    // Outras configurações existentes
+};
 
-export default nextConfig;
+module.exports = nextConfig;
