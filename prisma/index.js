a981524e401f6053e56b5638de3239eb6c197c3c
Lucca Nunes <lucca.nunes@ifood.com.br>
2024-11-26 16:57:59 -0300

    muda porta

--- prisma/index.js
+++ prisma/index.js
@@ -191,6 +191,6 @@ app.get('/locais/:id', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+app.listen(3001, () => {
+  console.log('Servidor rodando na porta 3001');
 });
